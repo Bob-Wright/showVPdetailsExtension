79687c5731eab19f2ac28b4ce0fb5b9df89485a7
Bob Wright <itzbobwright@gmail.com>
2021-09-12 13:54:37 -0400

    Add files via upload

--- showVPdetails.js
+++ showVPdetails.js
@@ -2,12 +2,27 @@
 // 
 (function () {
 	//showVPdetails;
-	window.addEventListener('resize', showVPdetails);
-	setTimeout(showVPdetails, 50);
-	//showVPdetails;
+	var keyCode = '';
+	var hkey = "r";
+	setTimeout(showVPdetails, 5);
+	document.addEventListener("load", function(event) {
+		setTimeout(showVPdetails, 5);
+	});
+	document.addEventListener("keydown", function(event) {
+		event.preventDefault();
+		keyCode = event.key;
+		console.info('keycode = ' + keyCode);
+		console.info('hkey = ' + hkey);
+		if(keyCode == hkey) {
+			setTimeout(showVPdetails, 0);
+		};
+	});
+	setTimeout(function(){if(keyCode != '') {hkey = keyCode;} else {hkey = 'r';}}, 1500);
+	console.info('here we are');
 })();
 // show viewport size
 function showVPdetails() {
+  window.addEventListener('resize', showVPdetails);
   var viewportWidth = document.documentElement.clientWidth;
   var vpWidth = parseInt(viewportWidth);
   var viewportHeight = document.documentElement.clientHeight;
@@ -15,30 +30,45 @@ function showVPdetails() {
   var VPaspectRatio = viewportWidth / viewportHeight;
 	var VPaspectRounded = (Math.round(VPaspectRatio * 100)) / 100;
 	  // console.info("rounded VP aspect " + VPaspectRounded);
-	var vpdetails = viewportWidth + ' X ' + viewportHeight + ' px & w/h = ' + VPaspectRounded;
+	//var vpdetails = viewportWidth + ' X ' + viewportHeight + ' px & w/h = ' + VPaspectRounded;
+	var vpdetails = '<img src="images/isoblockBW48.png"> ' + viewportWidth + ' X ' + viewportHeight + ' px & w/h = ' + VPaspectRounded;
 	//var vpdetails = "test";
     var fontSize = '30px';
-	var dwidth = '450px';
-    if(vpWidth <= 840) { fontSize = '30px'; dwidth = '370px'; } else { fontSize = '35px'; dwidth = '450px'; };
+	var dwidth = '470px';
+    if(vpWidth <= 840) { fontSize = '30px'; dwidth = '410px'; } else { fontSize = '35px'; dwidth = '490px'; };
 	var element = document.getElementById("vpsize");
 	  if(element != null) {
 		element.parentNode.removeChild(element);
 	  };
-	const template = document.createElement('div');
+	var element = document.getElementById("vpstyle");
+	  if(element != null) {
+		element.parentNode.removeChild(element);
+	  };
+	var styletemplate = document.createElement('style');
+	styletemplate.setAttribute("id", "vpstyle");
+	styletemplate.setAttribute("class","dimensions");
+	var styleval = "#vpsize {";
+	styleval = styleval +  "font-size:" + fontSize + ";";
+	styleval = styleval +  "position: fixed;";
+	styleval = styleval +  "bottom: 0;";
+	styleval = styleval +  "left: 0;";
+	styleval = styleval +  "display: block;";
+	styleval = styleval +  "width: "+dwidth+";";
+	styleval = styleval +  "height: 40px;";
+	styleval = styleval +  "margin: 0 0 5vh 2vw;";
+	styleval = styleval +  "padding: 15px;";
+	styleval = styleval +  "color: #000;";
+	styleval = styleval +  "background: #0cf;";
+	styleval = styleval +  "z-index: 20;";
+	styleval = styleval +  "}";
+	styletemplate.innerHTML = styleval;
+	document.body.appendChild(styletemplate);
+	
+	var template = document.createElement('div');
 	template.setAttribute("id", "vpsize");
 	template.setAttribute("title", "Viewport Size and Aspect");
-	template.setAttribute("style","font-size:" + fontSize + ";z-index: 20;");
-	template.setAttribute("class","dimensions");
-	template.style.position = 'fixed';
-	template.style.bottom = 0;
-	template.style.left = 0;
-	template.style.display = 'block';
-	template.style.width = dwidth;
-	template.style.height = '40px';
-	template.style.margin = '0 0 60px 40px';
-	template.style.padding = '15px';
-	template.style.color = '#fff';
-	template.style.background = '#08c';
-	template.textContent = vpdetails;
+	setTimeout(function(){template.style.display = 'none';}, 5000);
+	// Notification icon.
+	template.innerHTML = vpdetails;
 	document.body.appendChild(template);
 };
